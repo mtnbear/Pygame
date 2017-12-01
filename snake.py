c0467e001ebe714a3c442388329da56d3af8fbd7
Brandon Marquart <hammersandhair@aol.com>
2017-12-01 15:01:46 -0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,2 +1,4 @@
-# Pygame
-This is a python project for a school project its like the game of snake but with different color dots that are worth different amounts of points 
+ class Player:
+    x = 10
+    y = 10 
+    speed = 1
