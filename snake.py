004091680001683bf77d4a47c04567e63052d807
Brandon Marquart <hammersandhair@aol.com>
2017-12-05 14:53:26 -0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -30,4 +30,49 @@
        
        def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowheight
+        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+        
+        pygame.display.set_caption('Pygame snake game')
+        self._running = false
+        def on_loop(self):
+         pass
+        
+        def on_render(self):
+         self._display_surf.fill((0,0,0))
+         self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
+         pygame.display.flip()
+         
+         def on_cleanup(self):
+          pygame.quit()
+          
+          def on_execute(self):
+           if self.on_init() == false:
+             self._running = false
+             
+             while( self._running ):
+              pygame.event.pump()
+              keys = pygame.key.get_pressed()
+              
+              if (key[K_RIGHT]):
+               self.player.moveRight()
+               
+               if (key[K_LEFT]):
+               self.player.moveLeft()
+               
+               if (key[K_UP]):
+                self.player.MoveUp()
+                
+                if (key[K_DOWN]):
+                 self.player.MoveDown()
+                 
+                 if (key[K_ECSCAPE]:
+                     self._running = false 
+                  
+                     
+                
+         
+                
+           
+                                                      
+                                                      
+                                               
