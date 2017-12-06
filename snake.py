b2e7e8e761f8ef2d00cda32594db4a7892910ca4
Brandon Marquart <hammersandhair@aol.com>
2017-12-06 14:53:55 -0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,36 +1,83 @@
  from.pygamelocals import*
  import time
  
+ class Apple:
+    x = 0
+    y = 0
+    step = 44
+ 
+    def __init__(self,x,y):
+        self.x = x * self.step
+        self.y = y * self.step
+ 
+    def draw(self, surface, image):
+        surface.blit(image,(self.x, self.y))
+ 
+ 
+ 
  class Player:
-      x = 10
-      y = 10
-      speed = 32                
-      direction = 0               
-         
-      def update(self):
-          if self.direction == 0:
-                     self.x = self.x + self.speed
-          if self.direction == 1:
-                     self.x = self.x - self.speed
-          if self.direction == 2:
-                     self.y = self.y - self.speed
-          if self.direction == 3:
-                     self.y = self.y + self.speed
-           
-                                                      
-          def moveRight(self):
-                     self.direction = 0
-          
-          def moveLeft(self):
-                     self.direction = 1
-                     
-          def moveUp(self):
-                     self.direction = 2
-                     
-          def moveDown(self):
-                     self.direction = 3
+    x = []
+    y = []
+    step = 44
+    direction = 0
+    length = 3
+ 
+    updateCountMax = 2
+    updateCount = 0
+ 
+    def __init__(self, length):
+       self.length = length
+       for i in range(0,length):
+           self.x.append(0)
+           self.y.append(0)
+ 
+    def update(self):
+ 
+        self.updateCount = self.updateCount + 1
+        if self.updateCount &gt; self.updateCountMax:
+ 
+            # update previous positions
+            for i in range(self.length-1,0,-1):
+                print "self.x[" + str(i) + "] = self.x[" + str(i-1) + "]"
+                self.x[i] = self.x[i-1]
+                self.y[i] = self.y[i-1]
+ 
+            # update position of head of snake
+            if self.direction == 0:
+                self.x[0] = self.x[0] + self.step
+            if self.direction == 1:
+                self.x[0] = self.x[0] - self.step
+            if self.direction == 2:
+                self.y[0] = self.y[0] - self.step
+            if self.direction == 3:
+                self.y[0] = self.y[0] + self.step
+ 
+            self.updateCount = 0
+ 
+ 
+    def moveRight(self):
+        self.direction = 0
+ 
+    def moveLeft(self):
+        self.direction = 1
+ 
+    def moveUp(self):
+        self.direction = 2
+ 
+    def moveDown(self):
+        self.direction = 3 
+ 
+    def draw(self, surface, image):
+        for i in range(0,self.length):
+            surface.blit(image,(self.x[i],self.y[i])) 
   
-       
+    class Game:
+    
+    def isCollision(self,x1,y1,x2,y2,bsize):
+        if x1 >= x2 and x1 <= x2 + bsize:
+            if y1 >= y2 and y1 <= y2 + bsize:
+                return True
+        return False   
        
      class App:
       
@@ -91,5 +138,10 @@
 if __name__ == "__main__" :
                      theApp = App()
                      theApp.on_execute()
+                     
+
+                     
+                     
+                     
                      
        
