42684fd3a432e204c4498cbb7f0766e460e02501
Brandon Marquart <hammersandhair@aol.com>
2017-12-06 15:03:37 -0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -80,68 +80,93 @@
         return False   
        
      class App:
-      
-      windowWidth = 800
-      windowHeight = 600
-      player = 0
-      
-      def __init__(self):
-       self.running = true
-       self.display_surf = true
-       self.image_surf = true
-       self.play = Player()
-       
-       def on_init(self):
+ 
+    windowWidth = 800
+    windowHeight = 600
+    player = 0
+    apple = 0
+ 
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self._image_surf = None
+        self._apple_surf = None
+        self.game = Game()
+        self.player = Player(3) 
+        self.apple = Apple(5,5)
+ 
+    def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
-        
-        pygame.display.set_caption('Pygame snake game')
-        self._running = false
-        def on_loop(self):
-         pass
-        
-        def on_render(self):
-         self._display_surf.fill((0,0,0))
-         self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
-         pygame.display.flip()
-         
-         def on_cleanup(self):
-          pygame.quit()
-          
-          def on_execute(self):
-           if self.on_init() == false:
-             self._running = false
-             
-             while( self._running ):
-              pygame.event.pump()
-              keys = pygame.key.get_pressed()
-              
-              if (key[K_RIGHT]):
-               self.player.moveRight()
-               
-               if (key[K_LEFT]):
-               self.player.moveLeft()
-               
-               if (key[K_UP]):
-                self.player.MoveUp()
-                
-                if (key[K_DOWN]):
-                 self.player.MoveDown()
-                 
-                 if (key[K_ECSCAPE]:
-                     self._running = false 
-                     self.on_loop()
-                     self.on_render()
-                     
-                     time.sleep (50.0 / 1000.0);
-                   self.on_cleanup()
-if __name__ == "__main__" :
-                     theApp = App()
-                     theApp.on_execute()
-                     
+ 
+        pygame.display.set_caption('Pygame pythonspot.com example')
+        self._running = True
+        self._image_surf = pygame.image.load("block.jpg").convert()
+        self._apple_surf = pygame.image.load("block.jpg").convert()
+ 
+    def on_event(self, event):
+        if event.type == QUIT:
+            self._running = False
+ 
+    def on_loop(self):
+        self.player.update()
+ 
+      
+        for i in range(0,self.player.length):
+            if self.game.isCollision(self.apple.x,self.apple.y,self.player.x[i], self.player.y[i],44):
+                self.apple.x = randint(2,9) * 44
+                self.apple.y = randint(2,9) * 44
+                self.player.length = self.player.length + 1
+ 
+ 
 
-                     
-                     
-                     
-                     
-       
+        for i in range(2,self.player.length):
+            if self.game.isCollision(self.player.x[0],self.player.y[0],self.player.x[i], self.player.y[i],40):
+                print("You lose! Collision: ")
+                print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
+                print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + str(self.player.y[i]) + ")")
+                exit(0)
+ 
+        pass
+ 
+    def on_render(self):
+        self._display_surf.fill((0,0,0))
+        self.player.draw(self._display_surf, self._image_surf)
+        self.apple.draw(self._display_surf, self._apple_surf)
+        pygame.display.flip()
+ 
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            pygame.event.pump()
+            keys = pygame.key.get_pressed() 
+ 
+            if (keys[K_RIGHT]):
+                self.player.moveRight()
+ 
+            if (keys[K_LEFT]):
+                self.player.moveLeft()
+ 
+            if (keys[K_UP]):
+                self.player.moveUp()
+ 
+            if (keys[K_DOWN]):
+                self.player.moveDown()
+ 
+            if (keys[K_ESCAPE]):
+                self._running = False
+ 
+            self.on_loop()
+            self.on_render()
+ 
+            time.sleep (50.0 / 1000.0);
+        self.on_cleanup()
+ 
+if __name__ == "__main__" :
+    theApp = App()
+    theApp.on_execute()
